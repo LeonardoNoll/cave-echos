ce34aec82471e34a3776007079b7061a87d536b1
Leonardo Noll Cruz <leonoll8@hotmail.com>
2024-07-21 00:17:06 -0300

    base feita, fazer o JS da som

---
+++ src/ts/audioPlayer.ts
@@ -0,0 +1,8 @@
+let caveSound = new Audio('https://github.com/LeonardoNoll/cave-echos/blob/main/src/assets/sounds/Cave1.mp3'); 
+const playButton = document.getElementById('play');
+if (playButton) {
+    playButton.addEventListener('click', () => {
+    caveSound.play();
+    console.log('Audio is playing');
+    })
+};
